e8610da562cc662b2adeb8236e676ba4d49924be
Matthew Ruigrok <48931902+mruigrok@users.noreply.github.com>
2020-06-30 19:26:28 -0400

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -74,15 +74,16 @@ def get_model_data(screen_region, start_val, roi=True):
         #Focus on the road and less on surroundings, then re-size, re-colour and save to data
         if roi == True:
             screen = screen[200:500, 100:700]
-    
-        screen = cv2.resize(screen, (HEIGHT,WIDTH))
-        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
-        output_keys = keys_to_output(key_check())
-
-        
+            
+         '''
         screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
         #screen = roi(screen, [vertices])
         screen = cv2.resize(screen, (160,120))
+        '''
+        
+        screen = cv2.resize(screen, (HEIGHT,WIDTH))
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
+        output_keys = keys_to_output(key_check())
         training_data.append([screen, output_keys])
         
         print('Frame took {} seconds'.format(time.time()-loop_time))
